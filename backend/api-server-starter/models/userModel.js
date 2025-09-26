9a8aaa7464c94105216b3b7cb05ab306b2a63a12
john <juan.valenz.101@hotmail.com>
2025-09-26 11:56:45 +0300

    Add login and signup endpoints

--- backend/api-server-starter/models/userModel.js
+++ backend/api-server-starter/models/userModel.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const validator = require("validator");
 
 const Schema = mongoose.Schema;
 
@@ -36,5 +38,56 @@ const userSchema = new Schema(
   },
   { timestamps: true, versionKey: false  }
 );
+
+userSchema.statics.signup = async function (name, email, password, phone_number, gender, date_of_birth, membership_status) {
+  if ((!name, !email || !password || !phone_number || !gender || !date_of_birth || !membership_status)) {
+    throw Error("Please add all fields");
+  }
+  if (!validator.isEmail(email)) {
+    throw Error("Email not valid");
+  }
+  if (!validator.isStrongPassword(password)) {
+    throw Error("Password not strong enough");
+  }
+
+  const userExists = await this.findOne({ email });
+
+  if (userExists) {
+    throw new Error("User already exists");
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const user = await this.create({
+    name,
+    email,
+    password: hashedPassword,
+    phone_number,
+    gender,
+    date_of_birth,
+    membership_status,
+  });
+
+  return user;
+};
+
+userSchema.statics.login = async function (email, password) {
+  if (!email || !password) {
+    throw Error("All fields must be filled");
+  }
+
+  const user = await this.findOne({ email });
+  if (!user) {
+    throw Error("Incorrect email");
+  }
+
+  const match = await bcrypt.compare(password, user.password);
+  if (!match) {
+    throw Error("Incorrect password");
+  }
+
+  return user;
+};
 
 module.exports = mongoose.model("User", userSchema);
